970ee4f3ad6bfb9d26dfbc9cfd59bf903f370a9b
Fabian Schürer <fabian.schuerer@googlemail.com>
2016-01-10 22:06:15 +0100

    Moved code into module flink-visual-core

---
+++ flink-visual-core/src/main/java/org/tuberlin/de/common/model/abstracts/datasink/print/AbstractPrintDataSinkComponent.java
@@ -0,0 +1,11 @@
+package org.tuberlin.de.common.model.abstracts.datasink.print;
+
+import org.tuberlin.de.common.model.interfaces.datasink.DataSinkComponent;
+import org.tuberlin.de.common.model.abstracts.datasink.AbstractDataSinkComponent;
+
+/**
+ * Created by Malcolm-X on 26.12.2015.
+ */
+public abstract class AbstractPrintDataSinkComponent extends AbstractDataSinkComponent implements DataSinkComponent {
+
+}
--- src/test/java/org/tuberlin/de/test/model/ModelTest.java
+++
@@ -1,7 +0,0 @@
-package org.tuberlin.de.test.model;
-
-/**
- * Created by Malcolm-X on 26.12.2015.
- */
-public class ModelTest {
-}
